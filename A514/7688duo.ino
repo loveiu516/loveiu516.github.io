9e38888b041c9e313b126fb062a02f93d65bde7c
yoyoman <kcrt369@gmail.com>
2017-05-01 00:42:44 +0800

    Add files via upload

---
+++ A514/7688duo.ino
@@ -0,0 +1,86 @@
+#include "DHT.h"
+DHT dht1(4, DHT22);
+
+const int light1 = 2;
+const int light2 = 3;
+
+String light1on = "light1on";
+String light1off = "light1off";
+String light2on = "light2on";
+String light2off = "light2off";
+String getsensor = "getsensor";
+
+void setup()
+{
+  Serial1.begin(57600);
+  Serial.begin(9600);
+  dht1.begin();
+}
+
+void loop()
+{
+  String command = "";
+  while (Serial1.available()) {
+    int v = Serial1.read();
+    Serial.println(v, DEC);
+    if ( v != -1) {
+      command += (char)v;
+      if (command.equals(getsensor)) {
+        command = "";
+        thsensor();
+      }
+      if (command.equals(light1on)) {
+        command = "";
+        flight1on();
+      }
+      if (command.equals(light1off)) {
+        command = "";
+        flight1off();
+      }
+      if (command.equals(light2on)) {
+        command = "";
+        flight2on();
+      }
+      if (command.equals(light2off)) {
+        command = "";
+        flight2off();
+      }
+    }
+  }
+}
+
+void thsensor()
+{
+  float h1 = dht1.readHumidity();
+  float t1 = dht1.readTemperature();
+
+  if (h1 > 1 && t1 > 1) {
+    Serial1.println(",hh," + String(h1));
+    delay(1000);
+    Serial1.println(",tt," + String(t1));
+  }
+}
+
+void flight1on()
+{
+  pinMode(light1, OUTPUT);
+  digitalWrite(light1, LOW);
+}
+
+void flight1off()
+{
+  pinMode(light1, OUTPUT);
+  digitalWrite(light1, HIGH);
+}
+
+void flight2on()
+{
+  pinMode(light2, OUTPUT);
+  digitalWrite(light2, LOW);
+}
+
+void flight2off()
+{
+  pinMode(light2, OUTPUT);
+  digitalWrite(light2, HIGH);
+}
